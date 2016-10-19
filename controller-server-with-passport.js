29085d9e7af1f8f6161a4360bd192c6404f3796f
Brian Davis <herobd@gmail.com>
2016-10-19 14:59:06 -0600

    xxx

--- controller-server-with-passport.js
+++ controller-server-with-passport.js
@@ -21,8 +21,8 @@ var Database = require('./database')();
 var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
 
-var spottingaddon = require("./cpp/build/Debug/spottingaddon");
-//var spottingaddon = require("./cpp/build/Release/spottingaddon")
+//var spottingaddon = require("./cpp/build/Debug/spottingaddon");
+var spottingaddon = require("./cpp/build/Release/spottingaddon")
 
 numberOfTests=2;
 
